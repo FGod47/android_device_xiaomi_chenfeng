0d25c945063cefc3e35f4afc797d557c9708a62e
FGod47 <118654823+FGod47@users.noreply.github.com>
2024-10-08 13:43:05 +0530

    Update and rename omni_chenfeng.mk to twrp_chenfeng.mk

--- twrp_chenfeng.mk
+++ twrp_chenfeng.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from chenfeng device
 $(call inherit-product, device/xiaomi/chenfeng/device.mk)
 
 PRODUCT_DEVICE := chenfeng
-PRODUCT_NAME := omni_chenfeng
+PRODUCT_NAME := twrp_chenfeng
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 24053PY09I
 PRODUCT_MANUFACTURER := xiaomi
